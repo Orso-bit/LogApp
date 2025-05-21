b0cf2de7c6463fe315a9542bc99e6ba7110c56a9
Orso-bit <giovannijr2000@gmail.com>
2025-05-21 15:04:44 +0200

    project

---
+++ Log/Log/LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogApp.swift
+//  Log
+//
+//  Created by Giovanni Jr Di Fenza on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Log/Log/Project.swift
@@ -0,0 +1,7 @@
+//
+//  Project.swift
+//  Log
+//
+//  Created by Giovanni Jr Di Fenza on 21/05/25.
+//
+
